3c6df1dc8c1b210624f2e3cf5470e6a7a6c51c7b
Dan Puttick <dputtick@users.noreply.github.com>
2016-11-07 17:38:03 -0500

    Progress

--- framework/framework.py
+++ framework/framework.py
@@ -3,33 +3,37 @@ DEBUG = False
 ROUTES = {}
 CACHED_ROUTES = {}
 
-## setup stuff
+# setup stuff
+
 
 def make_app():
     # compile all of the routes here
     pass
 
 
-def choose_server(servername = ""):
-    return default_server()
+def choose_server(servername=""):
+    return use_default_server()
 
 
 def use_default_server():
     try:
         from server import server
+        global SERVER
         SERVER = server
     except ImportError:
         raise RuntimeError
-    return threadpool_server
 
-## handling stuff
-
-def request_handler(environ, start_response)
-    pass
+# handling stuff
 
 
-def response_generator()
-    pass
+def request_handler(environ, start_response):
+    # decode the environ and figure out what we need
+    response = response_generator()
+    return [response]
+
+
+def response_generator():
+    return None
 
 
 def run(ip='localhost', port='8080'):
--- server/server.py
+++ server/server.py
@@ -3,17 +3,18 @@ This was originally threadpool_server.py
 '''
 
 import socket as s
-from threading import Thread
 from concurrent.futures import ThreadPoolExecutor as Executor
+from urllib.parse import parse_qs, urlparse
 
 
+# Constants
 DEBUG = True
+
 
 class ThreadPoolServer():
     def __init__(self, addr):
         self.addr = addr
         self.executor = Executor()
-
 
     def _create_listening_socket(self):
         self.listening_socket = s.socket()
@@ -21,41 +22,42 @@ class ThreadPoolServer():
         self.listening_socket.bind(self.addr)
         self.listening_socket.listen(10)
 
-
     def _handle_request(self, request):
         environ = self._make_environ(request)
-        self.app(environ, start_response)
-
+        start_response = self._start_response
+        result = self.app(environ, start_response)
+        return result
 
     def _client_connection(self, client, addr):
         while True:
             request = client.recv(1024)
             if not request:
                 break
-            self._handle_request(request)
-            response = str(page).encode() + b'\n'
+            response = self._handle_request(request)
+            response = str(response).encode('utf-8') + b'\n'
             client.send(response)
             client.close()
         if DEBUG:
             print("Client connection closed: ", addr)
 
-
     def _make_environ(self, request):
         request = request.decode('utf-8')
-        environ = None
+        environ = {}
+        environ['REQUEST_METHOD'] = None
+
+
+
         request_list = request.split("\r\n")[0].split(" ")
         request_dict = {'type': request_list[0], 'path': request_list[1]}
         return environ
 
     def _write_data(self, data):
-        # this is a callable that should write data
+        # this is a callable that should write data if needed (as per WSGI)
         pass
 
-
-    def _start_response(self, status, response_headers, exc_info=None)
+    def _start_response(self, status, response_headers, exc_info=None):
         response_header = "HTTP/1.1 200 OK\r\n\r\n"
         return self._write_data
-
 
     def serve(self, app):
         self.app = app
