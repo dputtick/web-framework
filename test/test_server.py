0cd641eb9f233502008872be9cc7b747fbd88167
Dan Puttick <dputtick@users.noreply.github.com>
2016-11-24 17:40:25 -0500

    Fixed issues with module importing in test

--- test/test_server.py
+++ test/test_server.py
@@ -1,5 +1,10 @@
 from server import server
 
 
-def test_assertion():
-    assert True
+def test_make_server():
+    server = make_server()
+    print(type(server))
+
+
+def make_server():
+    return server.ThreadPoolServer('localhost')
