8065934ff6bc13b75d0410dae3dba4ebbd170444
Dan Puttick <dputtick@users.noreply.github.com>
2016-11-24 15:57:15 -0500

    Added a route to demo app

--- demo_app/demo_app.py
+++ demo_app/demo_app.py
@@ -1,7 +1,13 @@
 import framework
 
 
+@framework.route('/')
+def homepage():
+    page = 'static/index.html'
+    return page
+
+
 if __name__ == '__main__':
     app = framework.make_app()
     framework.use_default_server()
-    framework.run(ip = 'localhost', port = "8080")
+    framework.run(ip='localhost', port="8080")
